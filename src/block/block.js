f421f1e7ffd51e9a473125d4cf7a53b9a006a1b9
Paul Allen <p.allen.art@gmail.com>
2018-09-15 22:29:27 -0600

    Update address on click.

--- src/block/block.js
+++ src/block/block.js
@@ -2,12 +2,27 @@
  * BLOCK: gutenberg-leaflet-map-block
  *
  * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
+ *
+ * Wishlist Options
+ *
+ * - Custom Colors
+ * - Custom Markers
+ * - Location, click or lookup
+ * - Zoom level
+ * - Map Height and Width
+ * - Static?
+ *
  */
 
 //  Import CSS.
 import './style.scss';
 import './editor.scss';
+
+// Geocoder
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+import 'leaflet-control-geocoder/dist/images/geocoder.png';
+import 'leaflet-control-geocoder/dist/images/throbber.gif';
+import 'leaflet-control-geocoder';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
@@ -32,18 +47,35 @@ registerBlockType( 'gutenberg-leaflet-map-block/block-gutenberg-leaflet-map-bloc
 	icon: 'location', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'embed', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
-		__( 'gutenberg-leaflet-map-block' ),
 		__( 'Map' ),
 		__( 'Leaflet JS' ),
 	],
 
-	edit: function() {
-		return new mapBlock();
+	attributes: {
+		mapLatLng: {
+			type: 'array',
+			default: [ 51.505, -0.09 ],
+		},
+		markerLatLng: {
+			type: 'array',
+			default: [ 51.505, -0.09 ],
+		},
+		address: {
+			type: 'string',
+			default: 'London, Greater London, England, United Kingdom',
+		},
+	},
+
+	edit: function( attributes, setAttributes ) {
+		return new mapBlock( attributes );
 	},
 
 	save: function( props ) {
 		return (
-			<div data-gutenberg-leaflet-map-block='{ "maxZoom": 18, "id": "mapbox.streets" }' ></div>
+			<div data-gutenberg-leaflet-map-block='{
+				"maxZoom": 18,
+				"id": "mapbox.streets"
+			}' ></div>
 		);
 	},
 } );
@@ -54,8 +86,16 @@ registerBlockType( 'gutenberg-leaflet-map-block/block-gutenberg-leaflet-map-bloc
 class mapBlock extends Component {
 	constructor( props ) {
 		super( props );
+		this.attributes = props.attributes;
 		this.mapContainer = React.createRef();
 		this.mapboxApiKey = window.gutenberg_leaflet_map_block.mapbox_api_key;
+
+		this.updateAddress = this.updateAddress.bind( this );
+	}
+
+	updateAddress( address ) {
+		const { setAttributes } = this.props;
+		setAttributes( address );
 	}
 
 	render() {
@@ -65,21 +105,72 @@ class mapBlock extends Component {
 		};
 
 		return (
-			<div style={ styles } ref={ this.mapContainer }></div>
+			<div>
+				<p>{ this.attributes.address }</p>
+				<div style={ styles } ref={ this.mapContainer }></div>
+			</div>
 		);
 	}
 
 	componentDidMount() {
-		this.map = L.map( this.mapContainer.current ).setView( [ 51.505, -0.09 ], 13 );
+		this.map = L.map( this.mapContainer.current ).setView( this.attributes.mapLatLng, 13 );
+		this.marker = L.marker( this.attributes.markerLatLng ).addTo( this.map );
 
+		// Set Tiles
 		L.tileLayer(
 			'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
 			{
 				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 				maxZoom: 18,
+				minZoom: 2,
 				id: 'mapbox.streets',
 				accessToken: this.mapboxApiKey,
 			}
 		).addTo( this.map );
+
+		// Geocoder search box
+		L.Control.geocoder( {
+			defaultMarkGeocode: false,
+		} )
+			.on( 'markgeocode', e => {
+				const latlng = [ e.geocode.center.lat, e.geocode.center.lng ];
+
+				// Set marker and map
+				this.marker.setLatLng( latlng );
+				this.map.setView( latlng );
+
+				// Save location
+				this.attributes.mapLatLng = latlng;
+				this.attributes.markerLatLng = latlng;
+				this.attributes.address = e.geocode.name;
+
+				this.updateAddress( this.attributes.address );
+			} )
+			.addTo( this.map );
+
+		// Click to set location
+		this.map.on( 'click', e => {
+			const latlng = [ e.latlng.lat, e.latlng.lng ];
+
+			this.marker.setLatLng( latlng );
+			this.attributes.mapLatLng = latlng;
+			this.attributes.markerLatLng = latlng;
+
+			// Get address name from latlng
+			L.Control.Geocoder.nominatim().reverse(
+				e.latlng,
+				18,
+				( results ) => {
+					// Save the address name
+					if ( ! results.hasOwnProperty( 0 ) ) {
+						this.attributes.address = 'unknown';
+					} else {
+						this.attributes.address = results[ 0 ].name;
+					}
+
+					this.updateAddress( this.attributes.address );
+				}
+			);
+		} );
 	}
 }
